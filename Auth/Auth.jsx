5df6950e63af1be64735d11f15ca61492cc10435
Aiden <x29587629@gmail.com>
2021-10-25 23:07:16 +0800

    don't set isAuthorized to false

--- Auth/Auth.jsx
+++ Auth/Auth.jsx
@@ -7,7 +7,7 @@ class Auth extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isAuthorized: false,
+            isAuthorized: true,
             username: 'Admin',
             role: 'USER',
             authpage: 'FileController'
